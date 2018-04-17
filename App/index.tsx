18460f15b2ad5ee9e4c2fe7c63ac753276f64e85
Ramon <ramirez.roberto45@gmail.com>
2018-04-17 18:44:31 -0500

    setup typescript and react

---
+++ App/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render((
+        <div>Hello World from REACT.JS hello</div>
+),document.getElementById("root"));
