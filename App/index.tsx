974bb6bb3bdf9814698965de9f05ed616ea83955
Ramon <ramirez.roberto45@gmail.com>
2018-05-20 20:01:38 -0500

    setup css bundling through webpack

--- App/index.tsx
+++ App/index.tsx
@@ -3,5 +3,5 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
-        <div>Hello World from REACT.JS hello</div>
+        <div id="test">Hello World from REACT.JS</div>
 ),document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 "use strict";
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const OptimizeCSSAssets = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
     entry: {
-        'main': './App/index.tsx'
+        'main': ['./App/index.tsx','./Assets/main.css']
     },
     output: {
         filename: '[name].js',
@@ -13,15 +15,28 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.css']
     },
-
+    
+    plugins: [
+        new OptimizeCSSAssets(),
+        new ExtractTextWebpackPlugin("[name].css")
+    ],
+    
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
                 loader: 'awesome-typescript-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ExtractTextWebpackPlugin.extract({
+                    use: ['css-loader'],
+                    fallback: 'style-loader'
+                })
             }
         ]
     }
